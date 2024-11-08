73c4e1e3b6b6ce9b1a89e7dad77239f6f1dc122c
isnenisalsa <isnaenisalsabela@gmail.com>
2024-11-08 11:43:27 +0800

    migrat table guru

--- database/migrations/2024_11_08_025805_create_guru_table.php
+++ database/migrations/2024_11_08_025805_create_guru_table.php
@@ -13,7 +13,20 @@ return new class extends Migration
     {
         Schema::create('guru', function (Blueprint $table) {
             $table->id();
+            $table->string("nik");
+            $table->string("nama");
+            $table->string("tempat_lahir");
+            $table->string("tanggal_lahir");
+            $table->string("jenis_kelamin");
+            $table->string("nama_ibu_kandung");
+            $table->string("agama");
+            $table->string("status_perkawinan");
+            $table->string("email");
+            $table->string("username")->default(nama);
+            $table->string("password");
+            $table->unsignedBigInteger("roles_id");
             $table->timestamps();
+            $table->foreign("roles_id")->references("id")->on("roles")->cascadeOnDelete()->cascadeOnUpdate();
         });
     }
 
